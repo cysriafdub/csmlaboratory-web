0497688969f12cbd06c0e39b0837f0efdb59c7bf
Jyre <yarejerilyn@gmail.com>
2023-10-01 13:25:07 +0800

    fixed dashboard style

--- react-js/laboratorymanagement/src/components/dashboard/dashboards.js
+++ react-js/laboratorymanagement/src/components/dashboard/dashboards.js
@@ -1,19 +1,13 @@
 import React from "react";
 import InventoryOverview from "./InventoryOverview";
 import InventorySummary from "./InventorySummary";
-import Header from "../header/header";
-import Nav from "../navbar/navbar";
 import './dashboard.css';
 
 function Dashboard() {
     return (
         <section className="content">
-            <div className="titleHeader">
-                <div className='titleName'>
-                    <InventorySummary />
-                    <InventoryOverview />
-                </div>
-            </div>
+            <InventorySummary />
+            <InventoryOverview />
         </section>
         
     );
